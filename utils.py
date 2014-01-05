c3da5e5b348bcb972edec51bc52d67abb26f729f
Andrew Rosenfeld <rosnfeld@gmail.com>
2014-01-05 18:04:48 -0500

    Doing a more proper unicode approach for cleaning up the files

--- utils.py
+++ utils.py
@@ -2,39 +2,37 @@ import zipfile
 import os.path
 import io
 
-# TODO operate on unicode here rather than raw bytes, as it's possible you operate on unaligned data
 
-
-def ensure_only_leading_byte_order_mark(raw_contents):
+def strip_additional_byte_order_marks(unicode_contents):
     """
     Creditor Reporting System data seems to be in utf-16, but with multiple "Byte Order Marks" (BOMs) per file.
     (probably due to cat'ing together several utf-16 files)
     There is generally only supposed to be a leading BOM for a Unicode file.
-    This function converts the file contents so that there is only a leading BOM.
+    This function converts the file contents (already decoded into unicode) so that there is only a leading BOM.
     """
-    bom_stripped_contents = raw_contents.replace('\xff\xfe', '')
-    return '\xff\xfe' + bom_stripped_contents
+    return unicode_contents.replace(u'\ufeff', '')
 
 
-def strip_nulls(raw_contents):
+def strip_nulls(unicode_contents):
     """
     CRS data seems to have null control characters (NUL) in various fields, which confuse pandas
     """
-    # need to specify double null, as it's UTF-16
-    return raw_contents.replace('\0\0', '')
+    return unicode_contents.replace('\0', '')
 
 
-def convert_to_unix_line_endings(raw_contents):
-    return raw_contents.replace('\r\n', '\n')
+def convert_to_unix_line_endings(unicode_contents):
+    return unicode_contents.replace('\r\n', '\n')
 
 
 def clean_crs_file(raw_contents):
     # yes, a little duplicative/wasteful in terms of performance, but easier to read/maintain
-    contents = raw_contents
-    contents = ensure_only_leading_byte_order_mark(contents)
-    contents = strip_nulls(contents)
-    contents = convert_to_unix_line_endings(contents)
-    return contents
+    unicode_contents = raw_contents.decode('utf-16')
+    unicode_contents = strip_additional_byte_order_marks(unicode_contents)
+    unicode_contents = strip_nulls(unicode_contents)
+    unicode_contents = convert_to_unix_line_endings(unicode_contents)
+    # Intentionally encode as utf-8, to be less of a pain.
+    # If you let python write the unicode to a file, it chooses this anyhow, I'm just trying to be explicit about it.
+    return unicode_contents.encode('utf-8')
 
 
 def process_zip_file(zip_path):
