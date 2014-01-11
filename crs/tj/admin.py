0919cd5f27633fd28133865697a4ecf67d6f4176
Andrew Rosenfeld <rosnfeld@gmail.com>
2014-01-11 18:52:48 -0500

    Starting on data model

--- crs/tj/admin.py
+++ crs/tj/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from tj.models import Query, QueryUnion, ManualExclusion
 
 # Register your models here.
+admin.site.register(Query)
+admin.site.register(QueryUnion)
+admin.site.register(ManualExclusion)
--- crs/tj/models.py
+++ crs/tj/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Query(models.Model):
+    """
+    Currently just models some search text we want persisted.
+    Will be fleshed out with more fields in due course.
+    """
+    text = models.CharField(max_length=64)
+
+    def __unicode__(self):
+        return self.text
+
+
+class ManualExclusion(models.Model):
+    """
+    Models a manual exclusion of a row returned by a Query
+    """
+    pandas_row_id = models.PositiveIntegerField()
+    query = models.ForeignKey(Query)
+
+    def __unicode__(self):
+        return unicode(self.pandas_row_id)
+
+
+class QueryUnion(models.Model):
+    """
+    Models the union of several Queries.
+    Note that the Query results, pre-manual exclusion, are unioned together,
+    and then the manual exclusions also get unioned together and applied,
+    so it's not exactly the "straight" union of Query results.
+    """
+    queries = models.ManyToManyField(Query)
+    name = models.CharField(max_length=64)
+
+    def __unicode__(self):
+        return self.name
