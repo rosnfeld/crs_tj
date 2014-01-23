fb1aeb81b608321a9a1319faed2d93031c726492
Andrew Rosenfeld <rosnfeld@gmail.com>
2014-01-22 22:16:31 -0500

    Re-arranging columns and filters

--- crs/tj/models.py
+++ crs/tj/models.py
@@ -24,7 +24,7 @@ class ManualExclusion(models.Model):
 
 # would be nice to have a proper enum in a static "code table",
 # but I think this is the django way of doing things
-CODE_FILTER_TYPES = ['recipient', 'donor', 'agency', 'purpose', 'sector', 'channel']
+CODE_FILTER_TYPES = ['recipient', 'donor', 'agency', 'channel', 'sector', 'purpose']
 CODE_FILTER_CHOICES = tuple((x, x) for x in CODE_FILTER_TYPES)
 
 
