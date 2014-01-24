2e2dae60dd11de1a17f3b050bc6fe4ef79e25d86
Andrew Rosenfeld <rosnfeld@gmail.com>
2014-01-24 11:00:05 -0500

    Trying to fix static file locations

--- crs/settings.py
+++ crs/settings.py
@@ -75,9 +75,11 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 # Static asset configuration
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_PATH, 'static'),
 )
