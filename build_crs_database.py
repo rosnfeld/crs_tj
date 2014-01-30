8bcef3515b2c8111a40ce017718d882efcce6c9b
Andrew Rosenfeld <rosnfeld@gmail.com>
2014-01-30 16:21:11 -0500

    Building code tables Resolves #11

--- build_crs_database.py
+++ build_crs_database.py
@@ -1,34 +1,71 @@
 """
-For building a PostgreSQL database tables to house CRS data for the web app.
+For building PostgreSQL database tables to house CRS data for the web app.
 """
 
 import psycopg2
+import os
+import pandas as pd
+
+# somewhat different than what is in models.py
+CODE_TABLES = ['donor', 'agency', 'recipient', 'region', 'incomegroup', 'flow', 'purpose', 'sector', 'channel']
 
 
 def get_db_connection(host, database, user, password):
     return psycopg2.connect(host=host, database=database, user=user, password=password)
 
 
-def create_crs_table(connection):
-    cursor = connection.cursor()
+# duplicated with query_processor.py
+def get_all_name_code_pairs(dataframe, filter_type):
+    """
+    Returns a dataframe containing ___code/___name pairs.
+    """
+    code_column = filter_type + 'code'
+    name_column = filter_type + 'name'
 
+    rows = dataframe[[code_column, name_column]].drop_duplicates()
+
+    # filter out missing codes (can happen for channel), and missing values will mean that pandas will have
+    # interpreted code column as float (so that it can use NaN), need to set as int
+    rows = rows.dropna()
+    rows[code_column] = rows[code_column].astype(int)
+
+    rows = rows.sort(name_column)
+
+    return rows.rename(columns={code_column: 'code', name_column: 'name'})
+
+
+def build_code_tables(cursor, dataframe):
+    for filter_type in CODE_TABLES:
+        table_name = filter_type
+        rows = get_all_name_code_pairs(dataframe, filter_type)
+
+        # these are the database table names, not the pandas dataframe column names
+        code_column = filter_type + 'code'
+        name_column = filter_type + 'name'
+
+        # create table and populate it
+        create_sql_template = 'CREATE TABLE {table_name} ({code_column} integer, {name_column} varchar(127));'
+        create_sql = create_sql_template.format(table_name=table_name, code_column=code_column, name_column=name_column)
+
+        cursor.execute(create_sql)
+
+        for i, row in rows.iterrows():
+            insert_sql = "INSERT INTO {table_name} VALUES (%(code)s, %(name)s);".format(table_name=table_name)
+            cursor.execute(insert_sql, {'code': row['code'], 'name': row['name']})
+
+
+def create_crs_table(cursor):
     sql = '''CREATE TABLE crs (
         year integer,
         donorcode integer,
-        donorname varchar (63),
         agencycode integer,
-        agencyname varchar (63),
         crsid varchar (63),
         projectnumber varchar (63),
         initialreport integer,
         recipientcode integer,
-        recipientname varchar (63),
         regioncode integer,
-        regionname varchar (63),
         incomegroupcode integer,
-        incomegroupname varchar (63),
         flowcode integer,
-        flowname varchar (63),
         bi_multi integer,
         category integer,
         finance_t integer,
@@ -57,11 +94,8 @@ def create_crs_table(connection):
         shortdescription text,
         projecttitle text,
         purposecode integer,
-        purposename varchar (63),
         sectorcode integer,
-        sectorname varchar (63),
         channelcode double precision,
-        channelname varchar (63),
         channelreportedname text,
         geography text,
         expectedstartdate varchar (63),
@@ -73,3 +107,20 @@ def create_crs_table(connection):
         PBA double precision
     );'''
 
+
+if __name__ == "__main__":
+    host = os.environ['POSTGRES_HOST']
+    database = os.environ['POSTGRES_DB']
+    user = os.environ['POSTGRES_USER']
+    password = os.environ['POSTGRES_PASSWORD']
+
+    connection = get_db_connection(host, database, user, password)
+    cursor = connection.cursor()
+
+    master_pickle_path = '/home/andrew/oecd/crs/processed/2014-01-30/all_data.pkl'
+    dataframe = pd.read_pickle(master_pickle_path)
+
+    build_code_tables(cursor, dataframe)
+    connection.commit()
+    cursor.close()
+    connection.close()
