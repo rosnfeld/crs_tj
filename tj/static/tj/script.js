70302344d94d7d81f956a051ad849c063a12c9f6
Andrew Rosenfeld <rosnfeld@gmail.com>
2014-02-15 17:21:47 -0500

    Starting to centralize JavaScript in a static file

---
+++ tj/static/tj/script.js
@@ -0,0 +1,16 @@
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+function initializeAJAX(csrfToken) {
+    $.ajaxSetup({
+        crossDomain: false, // obviates need for sameOrigin test
+        beforeSend: function (xhr, settings) {
+            if (!csrfSafeMethod(settings.type)) {
+                xhr.setRequestHeader("X-CSRFToken", csrfToken);
+            }
+        },
+        timeout: 0  // in milliseconds, 0 means "don't time out"
+    });
+}
